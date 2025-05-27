b5472ba8b938ead85fe5ae9ea2d570a8799af24a
Mikhail Deriabin <mikko.deriabin.work@gmail.com>
2025-05-27 17:47:26 +0300

    add project

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
-const lol = 1 + 2;
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
-console.log(lol);
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
